79ff179b989ed5c4a3cd8980d6ba91543059175b
Icetune <109099573+Icetune@users.noreply.github.com>
2023-01-25 15:45:04 +0100

    angular fire installed

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+  firebase: {
+    projectId: 'ring-of-f-aed68',
+    appId: '1:360075738309:web:b189bd2866076b2f9cc4ac',
+    storageBucket: 'ring-of-f-aed68.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCq5if4AYEEYABVHm0tnofAyg5FJmp0_Ps',
+    authDomain: 'ring-of-f-aed68.firebaseapp.com',
+    messagingSenderId: '360075738309',
+  }
+};
