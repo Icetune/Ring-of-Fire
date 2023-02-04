0a559e2a97bc4b9b49b8acafd3a9a0a3eef3ed36
Icetune <s.f.munk@gmx.de>
2023-02-04 16:12:28 +0100

    ng build & firebase init done, deploy doesnt work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
     production: true,
+    firebase: {
+        projectId: 'ring-of-fire-ea4f3',
+        appId: '1:197837684409:web:eca78a8dbb3039a7006568',
+        storageBucket: 'ring-of-fire-ea4f3.appspot.com',
+        locationId: 'europe-west',
+        apiKey: 'AIzaSyDMWbTE3q3KRsW9UF6D_l7zVJCntOoDKw0',
+        authDomain: 'ring-of-fire-ea4f3.firebaseapp.com',
+        messagingSenderId: '197837684409',
+      }
 };
