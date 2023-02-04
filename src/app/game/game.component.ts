cf3e3827b164a8870af35e7e007e9bd5662047b0
Icetune <109099573+Icetune@users.noreply.github.com>
2023-02-04 04:24:15 +0100

    old version somehow works

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,8 +4,8 @@ import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
-import { Firestore, collectionData, collection, addDoc, setDoc, doc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-game',
@@ -18,35 +18,70 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string = '';
 
-  games$: Observable<any>;  //Observable ist eine Veriable, die sich updated
+  // gameID: string;
 
-  constructor(private firestore: Firestore, public dialog: MatDialog) {
 
-    const coll = collection(this.firestore, 'games');
-    this.games$ = collectionData(coll);
-    this.games$.subscribe((game) => {
-      console.log('Game Update', game);
-    });
 
-  }
+
+
+  constructor(private router: ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) {}
+
 
 
 
   ngOnInit(): void {
     this.newGame();
+
+    this.router.params.subscribe((param) => {
+      console.log('Game ID: ', param['id']);
+
+      // this.gameID = param['id'];
+
+
+
+      this
+      .firestore
+      .collection("games")
+      .doc(param['id'])
+      .valueChanges()
+      .subscribe((game: any) => {
+        console.log('Game Update', game);
+        this.game.currentPlayer = game.currentPlayer;
+        this.game.playedCards = game.playedCards;
+        this.game.players = game.player;
+        this.game.stack = game.stack;
+      })
+  
+
+    });
+
+
+
+
+
+
+
+
+
+    // const coll = collection(this.firestore, 'games');
+    // console.log(coll);
+
+    // this.games$ = collectionData(coll);
+    // console.log(this.games$);
+
+    // this.games$.subscribe((game) => {
+    //   console.log('Game Update', game);
+    // });
+
   }
 
 
   async newGame() {
     this.game = new Game;
 
-    const coll = collection(this.firestore, 'games');
-    // console.log('Document written with ID:', coll);
+    // const coll = collection(this.firestore, 'games');
+    // await addDoc(coll, this.game.toJSON());
 
-    await addDoc(coll, this.game.toJSON() );
-    
-    // let gameInfo = await addDoc(coll, { game: this.game.toJSON() });
-    // console.log(gameInfo);
   }
 
 
